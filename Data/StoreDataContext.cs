ce082e38ed59aa902732aaaa572ebf0c01e8470d
cadu-absistemas <cadu@absistemas.com.br>
2020-11-18 14:40:21 -0300

    creating maps

---
+++ Data/StoreDataContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProductCatalog_API.Data.Maps;
+using ProductCatalog_API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductCatalog_API.Data
+{
+    public class StoreDataContext: DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=prodcat;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.ApplyConfiguration(new ProductMap());
+            builder.ApplyConfiguration(new CategoryMap());
+        }
+    }
+}
--- Models/Product.cs
+++ Models/Product.cs
@@ -9,6 +9,7 @@ namespace ProductCatalog_API.Models
     {
         public int Id { get; set; }
         public string Title { get; set; }
+        public string Description { get; set; }
         public decimal Price { get; set; }
         public int Quantity { get; set; }
         public string Image { get; set; }
